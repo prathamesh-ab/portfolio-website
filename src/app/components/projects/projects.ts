5a193cb1f81affc161f2042d180aa43088bb6467
prathamesh-ab <prathamesh.bhanse18@gmail.com>
2025-09-24 00:29:02 +0530

    change reverted

--- src/app/components/projects/projects.ts
+++ src/app/components/projects/projects.ts
@@ -21,7 +21,7 @@ export class Projects implements OnInit {
       iconClass: 'fas fa-envelope',
       gradient: 'linear-gradient(135deg, #667eea 0%, #764ba2 100%)',
       category: 'web-app',
-      status: 'Deployed',
+      status: 'in-progress',
       liveUrl: 'https://mailcraft.prathameshbhanse.live/',
       githubUrl: '#',
       features: [
